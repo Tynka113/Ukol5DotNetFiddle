cdfb25fa10cd8ae3a82dc57484f2f2d973b58b4d
Tynka113 <macha@DESKTOP-TMSSHNL>
2023-11-08 21:21:29 +0100

    Add project files.

---
+++ Ukol5DotNetFiddle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // 1. Vypiš aktuální datum a čas, nemusíš řešit, v kterém je to časovém pásmu.
+
+        // 2. Vytvoř proměnnou typu DateTime a ulož do ní svoje datum narození. Potom vypiš, kolik dnů od té doby uteklo.		
+
+        // 3. Vytvoř list stringů a vlož do něj 5 různých hodnot.
+
+        // 4. Smaž z tohoto listu libovolnou hodnotu.
+
+        // 5. Zjisti, jestli tento list obsahuje nějakou hodnotu pomocí list metody Contains ... zkus na internetu zjistit, jak ji použít.
+
+        // 6. Vypiš do konzole, kolik je v tom listu prvků a připoj k tomu všechny ty hodnoty (např: "2: modra, zelena").
+
+        // 7. Vytvoř slovník, kde klíčem bude položka nákupu (string) a hodnotou cena té položky, a vlož nějaké hodnoty (např: <"chleba", 20>).
+
+        // 8. Zjisti, jestli slovník obsazhuje nějakou konkrétní potravinu a pokud ano, vypiš její cenu, pokud ne, vypiš, že není.
+
+        // 9. Řekněme, že už jsi do slovníku přidala např. chleba a zjistila, že máš v nákupní tašce ještě jeden -> uprav hodnotu k tomu klíči tak, aby obsahovala hromadnou cenu za všechny stejné položky.
+    }
+}
